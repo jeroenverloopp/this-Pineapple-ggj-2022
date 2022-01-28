7f114cc5ec1f91414c33d3ac5987686fbd22ad33
DavidvanderSar <davidvandersar@gmail.com>
2022-01-28 20:02:52 +0100

    Creatures - Create Base v 0.1

---
+++ Assets/Scripts/BaseCreatureData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCreatureData : ScriptableObject
+{
+    public float MoveSpeed;
+
+    public List<>
+}
