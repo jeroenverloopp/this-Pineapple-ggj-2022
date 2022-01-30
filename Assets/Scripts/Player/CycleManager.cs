2a79f62d413dda740932e1a363cc9005370a4a2d
DavidvanderSar <davidvandersar@gmail.com>
2022-01-30 15:12:08 +0100

    Added shift change event

--- Assets/Scripts/Player/CycleManager.cs
+++ Assets/Scripts/Player/CycleManager.cs
@@ -1,61 +1,70 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
-public class CycleManager : MonoBehaviour
+namespace CycleManager
 {
-    public Transform ClockTransform;
-
-    public Image healthCircle;
-
-    public enum Phases { Day, Night }
-
-    public Phases CurrentPhase;
-
-    public float timePerPhase = 36f;
-
-    public float currentTime = 0f;
-
-    public int passedPhases;
-
-    public string currentClockTime;
-
-    [SerializeField]
-    private TeethControl _player;
-
-    // Start is called before the first frame update
-    void Start()
+    public class CycleManager : MonoBehaviour
     {
-    }
+        public Action<Phases> OnCycleShift;
 
-    // Update is called once per frame
-    void Update()
-    {
-        currentTime += Time.deltaTime;
+        public Transform ClockTransform;
 
-        passedPhases = (int)(currentTime / timePerPhase);
+        public Image healthCircle;
 
-        CurrentPhase = passedPhases % 2 == 0 ? Phases.Day : Phases.Night;
+        public Phases CurrentPhase;
 
-        currentClockTime = GetCurrentClockTime();
+        public float timePerPhase = 36f;
 
-        var restForRotation = (currentTime % (timePerPhase * 2));
-        var clockRotation = ((restForRotation / (timePerPhase * 2)) * 360) - 90;
+        public float currentTime = 0f;
 
-        ClockTransform.rotation = Quaternion.Euler(0, 0, clockRotation);
+        public int passedPhases;
 
-        if (_player != null)
+        public string currentClockTime;
+
+        [SerializeField]
+        private TeethControl _player;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            healthCircle.fillAmount = _player.Hunger / 100f;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            currentTime += Time.deltaTime;
+
+            var previousPassed = passedPhases;
+            passedPhases = (int)(currentTime / timePerPhase);
+            CurrentPhase = passedPhases % 2 == 0 ? Phases.Day : Phases.Night;
+
+            if (passedPhases > previousPassed)
+                OnCycleShift.Invoke(CurrentPhase);
+
+            currentClockTime = GetCurrentClockTime();
+
+            var restForRotation = (currentTime % (timePerPhase * 2));
+            var clockRotation = ((restForRotation / (timePerPhase * 2)) * 360) - 90;
+
+            ClockTransform.rotation = Quaternion.Euler(0, 0, clockRotation);
+
+            if (_player != null)
+            {
+                healthCircle.fillAmount = _player.Hunger / 100f;
+            }
+        }
+
+        private string GetCurrentClockTime()
+        {
+            var time = (6 + (currentTime / timePerPhase * 12)) % 24;
+            var hours = (int)time;
+            var minutes = (int)((time % 1) * 100 * 0.6);
+            string hoursString = hours < 10 ? "0" + hours : hours.ToString();
+            string minutesString = minutes < 10 ? "0" + minutes : minutes.ToString();
+            return $"{hoursString}:{minutesString}";
         }
     }
 
-    private string GetCurrentClockTime()
-    {
-        var time = (6 + (currentTime / timePerPhase * 12)) % 24;
-        var hours = (int)time;
-        var minutes = (int)((time % 1) * 100 * 0.6);
-        string hoursString = hours < 10 ? "0" + hours : hours.ToString();
-        string minutesString = minutes < 10 ? "0" + minutes : minutes.ToString();
-        return $"{hoursString}:{minutesString}";
-    }
+    public enum Phases { Day, Night };
 }
